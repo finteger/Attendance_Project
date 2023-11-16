a356cd9b514f48d1d54b99e0af2c8f70b1e10de9
finteger <nashassociatesinc@gmail.com>
2023-11-16 10:53:47 -0700

    My Second Commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const Student = require("./student.js");
 
 const app = express();
 const PORT = 3000;
@@ -22,22 +25,41 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.static("public/css"));
 app.use(express.static("public/images"));
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get("/register", (req, res) =>{
     res.render("register");
 });
 
+app.post("/register", async (req, res) => {
 
+     const {email, password, confirmPassword} = req.body;   
+     const user = await Student.findOne({email});
 
+    if(user){
+        res.status(400).send('Username already exists.  Please try again.');
+        return;
+    }
 
+    if(password != confirmPassword){
+        res.status(400).send("Passwords do not match.");
+        return;
+    }
 
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) =>{
 
+    const user = new Student({
+        email: req.body.email,
+        password: hashedPassword,
+    });
 
+    user.save();
 
-
-
-
+    });
+    
+}); 
 
 
 app.listen(PORT, () => {
